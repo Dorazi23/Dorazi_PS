7931cec861fcf4fda1ed9f2cd5273b4365d7b718
Dorazi23 <41503777+Dorazi23@users.noreply.github.com>
2025-04-02 13:48:08 +0900

    [level 2] Title: 주식가격, Time: 28.75 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -5,25 +5,5 @@ using namespace std;
 
 vector<int> solution(vector<int> prices) {
     vector<int> answer;
-    int sz = prices.size();
-    for(int i = 0; i < sz; i++){
-        int len = 0;
-        for(int j = i+1; j < sz; j++){
-            len++;
-            if(prices[i] > prices[j]){ 
-                break;
-            }
-        }
-        answer.push_back(len);
-    }
     return answer;
 }
-/*
-LIS
-i번째일 때 i+1 번째부터 끝까지 본인보다 연속된 큰 수의 개수를 센다
-만약에 바로 끊긴다면 유지를 한 것이므로 1초가 된다
-배열의 마지막은 0초이다
-
-vector<int> answer;
-    return answer;
-*/
