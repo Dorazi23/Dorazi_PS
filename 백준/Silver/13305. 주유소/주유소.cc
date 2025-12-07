5f312ab2c1be7276f03f26d8b5225e269611aae4
Dorazi23 <41503777+Dorazi23@users.noreply.github.com>
2025-12-07 20:10:44 +0900

    [Silver III] Title: 주유소, Time: 16 ms, Memory: 3684 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -9,42 +9,26 @@ using namespace std;
 using ll = long long;
 int n;
 vector<ll> len;
-vector<ll> price;
-ll last;
-ll answer;
 ll mn = 0x7f7f7f7f;
+ll tot;
 
 void input() {
     fastio; cin >> n;
     for (int i = 0; i < n-1; i++) {
         ll num; cin >> num;
         len.push_back(num);
-        last += num;
     }
     
     
     for (int i = 0; i < n; i++) {
-        ll num; cin >> num;
-        price.push_back(num);
-        if (i != n-1) mn = min(mn, num);
+        ll tmp; cin >> tmp;
+        if (tmp < mn) mn = tmp;
+        tot += len[i] * mn;
     }
-}
 
-ll past;
-void ans() {
-    for (int i = 0; i < n; i++) {
-        if (price[i] == mn) {
-            answer += mn * last;
-            cout << answer << endl;
-            return;
-        }
-        else answer += (price[i] * len[i]);
-        last = last - len[i];
-        
-    }
+    cout << tot << endl;
 }
 
 int main() {
     input();
-    ans();
 }
