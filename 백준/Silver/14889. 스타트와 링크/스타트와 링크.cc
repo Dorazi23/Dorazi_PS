36c278b2730e5a9eab7698b97c55329f23e81b5f
Dorazi23 <41503777+Dorazi23@users.noreply.github.com>
2026-01-26 19:40:02 +0900

    [Silver I] Title: 스타트와 링크, Time: 112 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -7,7 +7,38 @@
 #define fastio cin.tie(0)->sync_with_stdio(0)
 using namespace std;
 using ll = long long;
-int n, arr[25][25];
+int n, arr[25][25], mn = MAX;
+bool vis[25];
+
+void func (vector<int>& a, int start) {
+    if (a.size() == n/2) { // 값 계산
+        int res1 = 0;
+        for (auto i : a) {
+            for (auto j : a) {
+                res1 += arr[i][j];
+            }
+        }
+
+        int res2 = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (!vis[i] && !vis[j]) {
+                    res2 += arr[i][j];
+                }
+            }
+        }
+
+        mn = min(mn, abs(res1 - res2));
+    }
+
+    for (int i = start; i <= n; i++) {
+        a.push_back(i);
+        vis[i] = 1;
+        func(a, i+1);
+        vis[i] = 0;
+        a.pop_back();
+    }
+}
 
 void input () {
     cin >> n;
@@ -17,37 +48,8 @@ void input () {
         }
     }
 
-    vector<int> team, tmp;
-    for (int i = 1; i <= n; i++) team.push_back(i);
-    for (int i = 1; i <= n; i++) {
-        if (i <= n/2) tmp.push_back(1);
-        else tmp.push_back(0);
-    }
-    
-    int mn = MAX;
-    do {
-        vector<int> res1, res2;
-        for (int i = 0; i < n; i++) {
-            if (tmp[i]) res1.push_back(team[i]);
-            else res2.push_back(team[i]);
-        }
-
-        int score1 = 0;
-        for (auto i : res1) {
-            for (auto j : res1) {
-                score1 += arr[i][j];
-            }
-        }
-
-        int score2 = 0;
-        for (auto i : res2) {
-            for (auto j : res2) {
-                score2 += arr[i][j];
-            }
-        }
-        
-        mn = min(mn, abs(score1 - score2));
-    } while (prev_permutation(tmp.begin(), tmp.end()));
+    vector<int> v;
+    func(v, 1);
 
     cout << mn << endl;
 }
