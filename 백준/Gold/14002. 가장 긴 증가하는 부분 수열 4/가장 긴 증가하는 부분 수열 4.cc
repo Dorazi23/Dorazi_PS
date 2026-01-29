5a5dc801e28c9de8855689c5ac9f43b29340c639
Dorazi23 <41503777+Dorazi23@users.noreply.github.com>
2026-01-29 16:48:30 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -2,52 +2,45 @@
 #define endl '\n'
 #define X first
 #define Y second
-#define MAX 1000000003
+#define MAX 1000000
+#define INF 4e18
 #define fastio cin.tie(0)->sync_with_stdio(0)
 using namespace std;
 using ll = long long;
-int n; int arr[1005], dp[1005], pre[1005];
-vector<int> lis;
+vector<int> lis, v;
+int n, arr[1005], len[1005];
+
+void input () {
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    lis.push_back(arr[0]);
+    len[0] = 1;
+    for (int i = 1; i < n; i++) {
+        if (lis[lis.size()-1] < arr[i]) {
+            lis.push_back(arr[i]);
+            len[i] = lis.size();
+        }
+        else {
+            int idx = lower_bound(lis.begin(), lis.end(), arr[i]) - lis.begin();
+            lis[idx] = arr[i];
+            len[i] = idx + 1;
+        }
+    }
+
+    int res = lis.size();
+    cout << res << endl;
+    for (int i = n-1; i >= 0; i--) {
+        if (len[i] == res) {
+            res--;
+            v.push_back(i);   
+        }
+    }
+
+    for (int i = v.size()-1; i >= 0; i--) cout << arr[v[i]] << " ";
+}
 
 int main() {
     fastio;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    dp[0] = 1;
-    for (int i = 1; i < n; i++) {
-        dp[i] = 1;
-        for (int j = 0; j < i; j++) {
-            if(arr[j] < arr[i]){
-                dp[i] = max(dp[i], dp[j]+1);
-            }
-        }
-    }
-
-    int mxlen = 0, idx;
-    for (int i = n-1; i >= 0; i--) {
-        if(dp[i] > mxlen){
-            mxlen = dp[i];
-            idx = i;
-        }
-    }
-    lis.push_back(arr[idx]);
-    int len = mxlen;
-    while(len > 1){
-        //cout << arr[idx] << " " << len << endl;
-        for (int i = idx; i >= 0; i--) {
-            if(len-1 == dp[i] && arr[idx] > arr[i]){
-                len -= 1;
-                idx = i;
-                lis.push_back(arr[idx]);
-                break;
-            }
-        }
-    }
-    cout << mxlen << endl;
-    reverse(lis.begin(), lis.end());
-    for (auto i : lis) {
-        cout << i << " ";
-    }
+    input();
 }
