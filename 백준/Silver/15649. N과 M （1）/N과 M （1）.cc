896c843f092a46f5c71dc4b326e13f62ac3f6eff
Dorazi23 <41503777+Dorazi23@users.noreply.github.com>
2026-02-01 22:09:56 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -6,30 +6,18 @@
 #define fastio cin.tie(0)->sync_with_stdio(0)
 using namespace std;
 using ll = long long;
-vector<int> v;
-bool vis[100];
 int n, m;
-
-void func (int cnt) {
-    if (cnt == m) {
-        for (auto x : v) cout << x << " ";
-        cout << endl;
-        return;
-    }
-
-    for (int i = 1; i <= n; i++) {
-        if (vis[i]) continue;
-        v.push_back(i);
-        vis[i] = 1;
-        func (cnt+1);
-        vis[i] = 0;
-        v.pop_back();
-    }
-}
+vector<int> v;
 
 void input () {
     cin >> n >> m;
-    func (0);
+    for (int i = 1; i <= n; i++) v.push_back(i);
+
+    do {
+        for (int i = 0; i < m; i++) cout << v[i] << " ";
+        cout << endl;
+        reverse(v.begin()+m, v.end());
+    } while (next_permutation (v.begin(), v.end()));
 }
 
 int main() {
