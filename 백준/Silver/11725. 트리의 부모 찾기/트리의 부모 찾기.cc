b300693729a65bfeac43e871b0326748891352a0
Dorazi23 <41503777+Dorazi23@users.noreply.github.com>
2026-02-03 15:48:37 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 32 ms, Memory: 8060 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -1,32 +1,45 @@
 #include <bits/stdc++.h>
 #define endl '\n'
+#define X first
+#define Y second
+#define INF 4e18
+#define fastio cin.tie(0)->sync_with_stdio(0)
 using namespace std;
-vector<int> adj[100005];
-int p[100005];
+using ll = long long;
 int n;
+vector<int> adj[100005], parent(100005, -1);
 
-void dfs(int cur){
-    for (auto nxt : adj[cur]) {
-        if(p[cur] == nxt) continue;
-        p[nxt] = cur;
-        dfs(nxt);
+void bfs (int root) {
+    queue<int> q;
+    q.push(root);
+    parent[root] = 0;
+    while (!q.empty()) {
+        auto cur = q.front(); q.pop();
+        // cout << cur << endl;
+        for (auto nxt : adj[cur]) {
+            if (cur == nxt || parent[nxt] != -1) continue;
+            parent[nxt] = cur;
+            q.push(nxt);
+        }
+    }
+}
+
+void input () {
+    cin >> n;
+    for (int i = 0; i < n-1; i++) {
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    bfs(1);
+
+    for (int i = 2; i <= n; i++) {
+        cout << parent[i] << endl;
     }
 }
 
 int main() {
-    ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n; int t = n;
-    while(t--){
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-    for (int i = 1; i <= n; i++) {
-        p[i] = i;
-    }
-    dfs(1);
-    for (int i = 2; i <= n; i++) {
-        cout << p[i] << endl;   
-    }
+    fastio;
+    input();
 }
