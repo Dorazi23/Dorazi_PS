1e60465cb39b467c11cd723bd18464d32f253e1d
Dorazi23 <41503777+Dorazi23@users.noreply.github.com>
2026-02-03 18:56:18 +0900

    [Gold V] Title: 트리와 쿼리, Time: 52 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -2,45 +2,40 @@
 #define endl '\n'
 #define X first
 #define Y second
-#define MAX 1000000007
-#define INF 0x3f3f3f3f
 #define fastio cin.tie(0)->sync_with_stdio(0)
 using namespace std;
-using ull = unsigned long long;
 int n, r, q;
-vector<int> adj[100005];
-vector<int> p(100005, -1);
-vector<int> dp(100005, 1);
+vector<int> adj[100005], p(100005, -1);
+int sz[100005];
 
-void func (int cur) {
-    dp[cur] = 1;
+int dfs (int cur) {
     for (auto nxt : adj[cur]) {
-        if (nxt == p[cur]) continue;
-        p[nxt] = cur;
-        func(nxt);
-        dp[cur] += dp[nxt];
+        if (p[cur] == nxt) continue;
+        p[nxt] = cur; 
+        sz[cur] += dfs(nxt);
     }
+
+    return sz[cur];
 }
 
 void input () {
-    cin >> n >> r >> q;    
+    fill (sz, sz+100004, 1);
+    cin >> n >> r >> q;
     for (int i = 0; i < n-1; i++) {
         int u, v; cin >> u >> v;
         adj[u].push_back(v);
         adj[v].push_back(u);
-        if (u == r) p[v] = u;
     }
-    
-    func(r);
 
-    while (q--) {
-        int num; cin >> num;
-        cout << dp[num] << endl;
-    }
+    dfs(r); // 부모 배열 채우기
     
-} 
+    while (q--) {
+        int m; cin >> m;
+        cout << sz[m] << endl;
+    }
+}
 
 int main() {
-    fastio; 
+    fastio;
     input();
 }
