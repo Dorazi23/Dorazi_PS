5a61263c8d1779cac4184f09dea63a940f8bf724
Dorazi23 <41503777+Dorazi23@users.noreply.github.com>
2026-02-04 15:55:31 +0900

    [Gold V] Title: 노드사이의 거리, Time: 12 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -4,39 +4,45 @@
 #define Y second
 #define fastio cin.tie(0)->sync_with_stdio(0)
 using namespace std;
+int n, m;
 vector<pair<int,int>> adj[1005];
-int d[1005];
-const int INF = 0x3f3f3f;
-int res;
-void dijkstra(int val, int tar){
-    memset(d, INF, sizeof(d));
-    priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-    d[val] = 0; pq.push({d[val], val});
-    while(!pq.empty()){
-        auto cur = pq.top(); pq.pop();
-        if(d[cur.Y] != cur.X) continue;
-        if(cur.Y == tar){
-            res = d[cur.Y];
+bool vis[1005];
+
+void bfs (int st, int en) {
+    memset(vis, 0, sizeof(vis));
+    queue<pair<int,int>> q;
+    q.push({st, 0});
+    vis[st] = 1;
+    while (!q.empty()) {
+        auto cur = q.front(); q.pop();
+        if (cur.X == en) {
+            cout << cur.Y << endl;
             return;
-        } 
-        for (auto nxt : adj[cur.Y]) {
-            if(d[nxt.Y] <= d[cur.Y] + nxt.X) continue;
-            d[nxt.Y] = d[cur.Y] + nxt.X;
-            pq.push({d[nxt.Y], nxt.Y});
+        }
+        
+        for (auto nxt : adj[cur.X]) {
+            if (vis[nxt.X]) continue;
+            vis[nxt.X] = 1;
+            q.push({nxt.X, cur.Y + nxt.Y});
         }
     }
 }
 
-int main() {
-    int n, m, depth = 0; cin >> n >> m;
+void input () {
+    cin >> n >> m;
     for (int i = 0; i < n-1; i++) {
-        int u, v, w; cin >> u >> v >> w;
-        adj[u].push_back({w,v});
-        adj[v].push_back({w,u});
+        int u, v, d; cin >> u >> v >> d;
+        adj[u].push_back({v, d});
+        adj[v].push_back({u, d});
     }
-    while(m--){
-        int a, b; cin >> a >> b;
-        dijkstra(a, b);
-        cout << res << endl;        
+    
+    while(m--) {
+        int st, en; cin >> st >> en;
+        bfs (st, en);
     }
+}
+
+int main() {
+    fastio;
+    input();
 }
