4e8cb0ac55ab35f0602adf2fb55743ab6cfd6785
Dorazi23 <41503777+Dorazi23@users.noreply.github.com>
2026-02-05 16:45:59 +0900

    [Gold IV] Title: 트리, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.cc
+++ 백준/Gold/4803. 트리/트리.cc
@@ -3,59 +3,63 @@
 #define X first
 #define Y second
 #define fastio cin.tie(0)->sync_with_stdio(0)
+#define INF 4e18
 using namespace std;
-bool vis[1000];
-int flag = 0;
-vector<int> adj[1000];
-void dfs(int cur, int before){
-    for (auto nxt : adj[cur]) {
-        if(nxt == before) continue;
-        if(nxt != before && vis[nxt]){
-            flag = 1;
-            return;
+using ll = long long;
+int n, m;
+vector<bool> isCycle(505);
+vector<int> p(505, -1);
+
+int Find (int x) {
+    if (p[x] < 0) return x;
+    return p[x] = Find(p[x]);
+}
+
+bool Union (int u, int v) {
+    u = Find(u), v = Find(v);
+    if (u == v) {
+        isCycle[u] = isCycle[v] = 1;
+        return false;
+    }
+    if (p[u] > p[v]) swap(u, v);
+    if (p[u] == p[v]) p[u]--;
+    p[v] = u;
+
+    if (isCycle[u] || isCycle[v]) { // 합치려는 큰 그래프가 사이클이 존재 
+        // 사이클 x인 노드들도 사이클로 취급
+        isCycle[u] = isCycle[v] = 1;
+    }
+    return true;
+}
+
+void input () {
+    int loopNum = 0;
+    while (cin >> n >> m) {
+        if (n == 0 && m == 0) break;
+        int cnt = 0;
+        fill (p.begin(), p.end(), -1);
+        fill (isCycle.begin(), isCycle.end(), 0);
+        loopNum++;
+        
+        for (int i = 0; i < m; i++) {
+            int u, v; cin >> u >> v;
+            Union(u, v);
         }
-        vis[nxt] = 1;
-        dfs(nxt, cur);
+
+        // 사이클이 없는 루트 노드의 개수 세
+        for (int i = 1; i <= n; i++) {
+            // cout << i << ": " << p[i] << " " << isCycle[i] << endl;
+            if (p[i] < 0 && !isCycle[i]) cnt++;
+        }
+  
+        cout << "Case " << loopNum << ": ";
+        if (cnt > 1) cout << "A forest of " << cnt << " trees." << endl;
+        else if (cnt == 1) cout << "There is one tree." << endl;
+        else cout << "No trees." << endl;
     }
 }
 
 int main() {
     fastio;
-    int c = 1;
-    while(1){
-        memset(vis, 0, sizeof(vis)); 
-        int ans = 0;
-        int n, m; cin >> n >> m;
-        if(n == 0 && m == 0){ //exit
-            return 0;
-        }
-
-        while(m--){
-            int u, v; cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        for (int i = 1; i <= n; i++) {
-            flag = 0;
-            if(vis[i]) continue;
-            dfs(i, -1);
-            if(!flag){
-                ans++;
-            }
-        }
-        if(ans > 1){
-            cout << "Case " << c << ": " << "A forest of " << ans << " trees." << endl;
-        }
-        else if(ans == 1){
-            cout << "Case " << c << ": " << "There is one tree." << endl;
-        }
-        else{
-            cout << "Case " << c << ": " << "No trees." << endl;
-        }
-
-        for (int i = 1; i <= n; i++) { //reset
-            adj[i].clear();
-        }
-        c++;
-    }
+    input();
 }
