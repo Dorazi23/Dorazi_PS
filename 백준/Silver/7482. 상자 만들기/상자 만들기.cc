02b6bdbca44de19b53815e50980fbd8761020ee4
Dorazi23 <41503777+Dorazi23@users.noreply.github.com>
2026-02-16 16:04:04 +0900

    [Silver V] Title: 상자 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/7482. 상자 만들기/상자 만들기.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define X first
+#define Y second
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define MAX 1000000000
+#define INF 4e18
+using namespace std;
+using ll = long long;
+
+void input () {
+    double a; cin >> a;
+    cout << fixed << setprecision(10) << a / 6 << endl;
+}
+
+int main() {
+    fastio;
+    int t; cin >> t;
+    while (t--) {
+        input();   
+    }
+}
