6627df0bfd5200783abf92f2f5e890c9baf4904c
Dorazi23 <41503777+Dorazi23@users.noreply.github.com>
2026-02-26 17:06:25 +0900

    [Gold II] Title: 가운데를 말해요, Time: 24 ms, Memory: 2792 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
@@ -1,30 +1,38 @@
 #include <bits/stdc++.h>
-#define fastio cin.tie(0)->sync_with_stdio(0)
 #define endl '\n'
+#define X first
+#define Y second
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define INF 4e17
 using namespace std;
+using ll = long long;
+priority_queue<int> maxHeap;
+priority_queue<int, vector<int>, greater<int>> minHeap;
+int t;
+
+void input () {
+    cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        // 1. 최대 힙의 크기는 최소 힙과 같더나 1만큼 커야 함
+        if (maxHeap.empty() || maxHeap.size() == minHeap.size() ) {
+            maxHeap.push(n);
+        }
+        else minHeap.push(n);
+
+        // 2. 최대 힙 top <= 최소 힙 top
+        if ((!maxHeap.empty() && !minHeap.empty()) && maxHeap.top() > minHeap.top()) {
+            int a = maxHeap.top(); maxHeap.pop();
+            int b = minHeap.top(); minHeap.pop();
+            minHeap.push(a); 
+            maxHeap.push(b);
+        }
+
+        cout << maxHeap.top() << endl;
+    }
+}
 
 int main() {
     fastio;
-    priority_queue<int> maxHeap;
-    priority_queue<int, vector<int>, greater<int>> minHeap;
-    int t; cin >> t;
-    while(t--){
-        int n; cin >> n;
-        if(maxHeap.empty() || n <= maxHeap.top()){
-            maxHeap.push(n);
-        }
-        else{
-            minHeap.push(n);
-        }
-        
-        if(maxHeap.size() > minHeap.size() + 1){
-            minHeap.push(maxHeap.top());
-            maxHeap.pop();
-        }
-        else if(minHeap.size() > maxHeap.size()){
-            maxHeap.push(minHeap.top());
-            minHeap.pop();
-        }
-        cout << maxHeap.top() << endl;
-    }
+    input();
 }
