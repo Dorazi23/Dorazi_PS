2fd312da1ff994649a4dcfa508d2bdf71a6eb737
Dorazi23 <41503777+Dorazi23@users.noreply.github.com>
2026-03-05 23:08:35 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int n;
+long long d[105];
+int main() {
+    cin >> n;
+    d[1] = 1;
+    d[2] = 1;
+    for (int i = 3; i <= n; i++) {
+        d[i] = d[i-1] + d[i-2];
+    }
+    cout<<d[n];
+}
