9a9adb3032957136577eb83895b6b13a291ddfaa
Dorazi23 <41503777+Dorazi23@users.noreply.github.com>
2026-03-24 21:34:01 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 1280 ms, Memory: 187140 KB -BaekjoonHub

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.cc
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.cc
@@ -2,64 +2,77 @@
 #define endl '\n'
 #define X first
 #define Y second
-#define INF 0x3f3f3f
 #define fastio cin.tie(0)->sync_with_stdio(0)
+#define INF 0x4f4f4f4f
 using namespace std;
-
+using ll = long long;
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
 int n, m, k;
-int dx[] = {0,0,1,-1};
-int dy[] = {1,-1,0,0};
-char board[1005][1005];
-int dist[1005][1005][12][2];
-bool OutOfBound(int x, int y){
+int dist[1005][1005][15][3];
+string board[1005];
+
+bool oob (int x, int y) {
     return x < 0 || x >= n || y < 0 || y >= m;
 }
 
-int bfs(){
-    memset(dist, -1, sizeof(dist));
+void bfs () {
+    // 초기화
+    for (int i = 0; i < 1005; i++) {
+        for (int j = 0; j < 1005; j++) {
+            for (int k = 0; k < 15; k++) {
+                fill (dist[i][j][k], dist[i][j][k]+3, -1);
+            }
+        }
+    }
+    
+    // 낮은 0, 밤은 1
+    queue<tuple<int,int,int,bool>> q;
+    q.push({0, 0, 0, 0});
     dist[0][0][0][0] = 1;
-    queue<tuple<int, int, int, int>> q;
-    q.push({0,0,0,0}); 
-    while(!q.empty()){
-        auto [x, y, z, t] = q.front(); q.pop();
-        if(x == n-1 && y == m-1) return  dist[x][y][z][t];
-        int nextdist = dist[x][y][z][t] + 1;
+    
+    while (!q.empty()) {
+        auto [x, y, count, day] = q.front(); q.pop();
+
+        if (x == n-1 && y == m-1) {
+            cout << dist[x][y][count][day] << endl;
+            return;
+        }
+        
         for (int dir = 0; dir < 4; dir++) {
             int nx = x + dx[dir];
             int ny = y + dy[dir];
-            if(OutOfBound(nx, ny)) continue;
-            if(t == 0){ //낮일 때
-                if(board[nx][ny] == '0' && dist[nx][ny][z][1] == -1){ //벽이 아닌 곳에 처음갈 때
-                    dist[nx][ny][z][1] = nextdist;
-                    q.push({nx,ny,z,1});
-                }
-                if(board[nx][ny] == '1' && z < k && dist[nx][ny][z+1][1] == -1){ //벽인 곳에 처음갈 때
-                    dist[nx][ny][z+1][1] = nextdist;
-                    q.push({nx,ny,z+1,1});
-                }
+            if (oob(nx, ny)) continue; // 맵 밖이면 넘김 
+            if (board[nx][ny] == '0') { // 이동 가능
+                if (dist[nx][ny][count][!day] != -1) continue;
+                dist[nx][ny][count][!day] = dist[x][y][count][day] + 1;
+                q.push({nx, ny, count, !day});
             }
-            else{ //밤일 때 
-                if(board[nx][ny] == '0' && dist[nx][ny][z][0] == -1){ //벽이 아닌 곳에 처음갈 때
-                    dist[nx][ny][z][0] = nextdist;
-                    q.push({nx,ny,z,0});
+            // 다음이 벽이고, 부술 수 있을 때 
+            if (board[nx][ny] == '1' && count < k) {
+                if (!day) { // 현재 낮일 때 
+                    if (dist[nx][ny][count+1][1] != -1) continue;
+                    dist[nx][ny][count+1][1] = dist[x][y][count][0] + 1;
+                    q.push({nx, ny, count+1, 1});    
                 }
-                if(board[nx][ny] == '1' && z < k && dist[nx][ny][z+1][1] == -1){ //정지
-                    dist[x][y][z][0] = nextdist;
-                    q.push({x, y, z, 0});
+                else { // 현재 밤
+                    dist[x][y][count][!day] = dist[x][y][count][day] + 1;
+                    q.push({x, y, count, !day});
                 }
             }
         }
     }
-    return -1;
+
+    cout << -1 << endl;
+}
+
+void input () {
+    cin >> n >> m >> k;
+    for (int i = 0; i < n; i++) cin >> board[i];
+    bfs();
 }
 
 int main() {
     fastio;
-    cin >> n >> m >> k;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> board[i][j];
-        }
-    }
-    cout << bfs() << endl;
+    input();
 }
